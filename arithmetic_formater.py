e6248dd16170540b080abe0c23edfbfb3508e409
Gabriel Batista de Oliveira <128636983+Brithwulf@users.noreply.github.com>
2025-03-12 18:08:13 -0300

    Update arithmetic_formater.py

--- arithmetic_formater.py
+++ arithmetic_formater.py
@@ -41,4 +41,3 @@ def arithmetic_arranger(problems, show_answers=False):
     if show_answers:
         arranged_problems += '\n' + answers_line.rstrip()
     return arranged_problems
-print(f'\n{arithmetic_arranger(["98 + 35", "3801 - 2", "45 + 43", "123 + 49"])}')
